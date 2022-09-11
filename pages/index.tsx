b51335e1f84bc8625426b0ac1df11ae57e4836cb
X <>
2022-09-10 22:11:21 -0700

    tbd footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -24,18 +24,7 @@ const Home: NextPage = () => {
         <BonusList bonuses={tempBonuses} />
       </main>
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{" "}
-          <span className={styles.logo}>
-            <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
-      </footer>
+      <footer className="sticky bottom-0 bg-white italic">footer tbd</footer>
     </div>
   );
 };
