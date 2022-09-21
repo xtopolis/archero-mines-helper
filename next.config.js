a717092c2a88bd4c6a72f82b70043e7ff632591b
X <>
2022-09-20 19:55:07 -0700

    fix relative paths for debug vs ghpages

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
+const debug = process.env.NODE_ENV !== "production";
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     unoptimized: true,
   },
-  basePath: "/archero-mines-helper"
+  assetPrefix: debug ? "" : "/archero-mines-helper", 
 }
 
 module.exports = nextConfig
